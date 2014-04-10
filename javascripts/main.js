3624e54293f631c9fa04e2c774286ec291960289
paninapress <NPacifico25@gmail.com>
2014-04-10 02:28:31 -0700

    mkActive function for carousel nav

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,21 @@
 var myApp = angular.module('myApp', []);
+
+myApp.controller('mainCtrl', function($scope){
+  $scope.mkActive = function(n){
+    if (n === 'nav1') {
+      $scope.n1 = 'active';
+      $scope.n2 = '';
+      $scope.n3 = '';
+    }
+    else if (n === 'nav2') {
+      $scope.n1 = '';
+      $scope.n2 = 'active';
+      $scope.n3 = '';
+    }
+    else {
+      $scope.n1 = '';
+      $scope.n2 = '';
+      $scope.n3 = 'active';
+    }
+  };
+});
