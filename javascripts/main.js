77c0c006fa353ae1e49c99013b67320a79a267c6
paninapress <NPacifico25@gmail.com>
2014-04-22 15:49:33 -0700

    nav styling and added to mkActive func

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,9 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainCtrl', function($scope){
+myApp.controller('mainCtrl',['$scope', function($scope){
   $scope.n4 = 'hidden';
   $scope.mkActive = function(n){
+    // these are the main 3 nav controls
     if (n === 'nav1') {
       $scope.n1 = 'active';
       $scope.n2 = '';
@@ -10,19 +11,49 @@ myApp.controller('mainCtrl', function($scope){
       if ($scope.n4 === 'hidden'){
         $scope.n4 = '';
       }
-      else {$scope.n4 = 'hidden';}
+      else {
+        $scope.n4 = 'hidden';
+        resetProjectnavs();
+      }
     }
     else if (n === 'nav2') {
       $scope.n1 = '';
       $scope.n2 = 'active';
       $scope.n3 = '';
       $scope.n4 = 'hidden';
+      resetProjectnavs();
     }
-    else {
+    else if (n === 'nav3') {
       $scope.n1 = '';
       $scope.n2 = '';
       $scope.n3 = 'active';
       $scope.n4 = 'hidden';
+      resetProjectnavs();
+    }
+    // these are the project nav controls
+    else if (n === 'nav5'){
+      $scope.n5 = 'navHighlight';
+      $scope.n6 = null;
+      $scope.n7 = null;
+    }
+    else if (n === 'nav6'){
+      $scope.n5 = null;
+      $scope.n6 = 'navHighlight';
+      $scope.n7 = null;
+    }
+    else if (n === 'nav7'){
+      $scope.n5 = null;
+      $scope.n6 = null;
+      $scope.n7 = 'navHighlight';
+    }
+    else{
+      resetProjectnavs();
     }
   };
-});
+  var resetProjectnavs = function(){
+      $scope.n5 = null;
+      $scope.n6 = null;
+      $scope.n7 = null;
+  }
+
+}]);
